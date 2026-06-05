ace82fce2dbeb15a7ab193a678a49650f52bb409
Timo Lehnertz <timo.lehnertz@nerou.de>
2026-06-05 21:52:18 +0200

    ErrorFix derives for error

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,16 @@
 use std::{io, sync::Arc};
 
 #[derive(Debug, Clone, thiserror::Error)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 pub enum Error {
     #[error("io error: {0}")]
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serialize_debug"))]
+    #[cfg_attr(feature = "schemars", schemars(with = "String"))]
     Io(Arc<io::Error>),
     #[error("serial port error: {0}")]
+    #[cfg_attr(feature = "serde", serde(serialize_with = "serialize_debug"))]
+    #[cfg_attr(feature = "schemars", schemars(with = "String"))]
     SerialPort(#[from] serialport::Error),
     #[error("protocol error: {0}")]
     Protocol(String),
@@ -18,4 +22,13 @@ impl From<io::Error> for Error {
     fn from(error: io::Error) -> Self {
         Error::Io(Arc::new(error))
     }
+}
+
+#[cfg(feature = "serde")]
+fn serialize_debug<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: std::fmt::Debug,
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&format!("{value:?}"))
 }
